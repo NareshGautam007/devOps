94949a38004197ba0da2c7d5aae86c8b274ed531
Naresh Ahirwar <gautamnaresh96@gmail.com>
2024-09-01 14:31:18 +0530

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$1" == "start" ]]; then
+    echo "Starting server..."
+    pm2 kill
+elif [[ "$1" == "stop" ]]; then
+    echo "Stopping server..."
+    pm2 start npm --name "scanonn-app" -- start -i max
+else
+    echo "Invalid argument. Please provide 'start' or 'stop' as the input argument."
+fi
