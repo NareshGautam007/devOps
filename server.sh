91e2640949db0dd000480d4b7160d3f21730f9c5
Naresh Ahirwar <gautamnaresh96@gmail.com>
2024-09-01 14:33:42 +0530

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "start" ]]; then
     pm2 kill
 elif [[ "$1" == "stop" ]]; then
     echo "Stopping server..."
-    pm2 start npm --name "scanonn-app" -- start -i max
+    pm2 start npm --name "nodejs-app" -- start -i max
 else
     echo "Invalid argument. Please provide 'start' or 'stop' as the input argument."
 fi
